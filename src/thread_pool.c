fb28b61c386f55d22f3676eadf8dc15e29010e55
Sebastian Guzman <code@ivansegufer.com>
2022-04-10 12:57:09 -0500

    Feat complete pool

--- src/thread_pool.c
+++ src/thread_pool.c
@@ -19,3 +19,44 @@ ThreadPool thread_pool_constructor(int num_threads)
     thread_pool.signal = (pthread_cond_t)PTHREAD_COND_INITIALIZER;
     return thread_pool;
 }
+
+ThreadJob thread_job_constructor(thread_job_t job_function, void *args)
+{
+    ThreadJob job;
+    job.job = job_function;
+    job.args = args;
+    return job;
+}
+
+void thread_pool_destructor(ThreadPool *thread_pool)
+{
+    thread_pool->active = 0;
+    for (int i = 0; i < thread_pool->num_threads; i++)
+    {
+        pthread_cond_signal(&thread_pool->signal);
+    }
+    for (int i = 0; i < thread_pool->num_threads; i++)
+    {
+        pthread_join(thread_pool->pool[i], NULL);
+    }
+    free(thread_pool->pool);
+    queue_destructor(&thread_pool->work);
+}
+
+void *thread_job(void *arg)
+{
+    ThreadPool *thread_pool = (ThreadPool *)arg;
+    while (thread_pool->active == 1)
+    {
+        pthread_mutex_lock(&thread_pool->lock);
+        pthread_cond_wait(&thread_pool->signal, &thread_pool->lock);
+        ThreadJob job = *(ThreadJob *)thread_pool->work.peek(&thread_pool->work);
+        thread_pool->work.pop(&thread_pool->work);
+        pthread_mutex_unlock(&thread_pool->work);
+        if (job.job)
+        {
+            job.job(job.args);
+        }
+    }
+    return NULL;
+}
